0ac9e98717a8c06dd219a76f455141c151af8bfd
Jakhongir <jakhongirrasulov001@gmail.com>
2020-07-13 15:09:44 +0500

    Updates

---
+++ precache-manifest.971444febd6e66fbfe86b0b089c00a0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2626e236d930a7841f51352ddb61131",
+    "url": "/css-grid-project/index.html"
+  },
+  {
+    "revision": "80ec702d5bbc08780954",
+    "url": "/css-grid-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "854898f9c248e113e78b",
+    "url": "/css-grid-project/static/js/2.9e5d0260.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/css-grid-project/static/js/2.9e5d0260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ec702d5bbc08780954",
+    "url": "/css-grid-project/static/js/main.41c23f1a.chunk.js"
+  },
+  {
+    "revision": "8ff58396c199900986b7",
+    "url": "/css-grid-project/static/js/runtime-main.949847d9.js"
+  }
+]);
